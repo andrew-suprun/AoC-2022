a04c3f7b82cd2ea462e302bf2ea44922372e2c60
Andrew Suprun <andrew.suprun@gmail.com>
2022-12-02 00:52:34 -0500

    refactor.2 day02

--- day02.jl
+++ day02.jl
@@ -1,6 +1,3 @@
-function day02(path, scores)
-    println(sum(scores[line] for line in readlines(path)))
-end
-
+day02(path, scores) = println(sum(scores[line] for line in readlines(path)))
 day02(ARGS[1], Dict("A X" => 4, "A Y" => 8, "A Z" => 3, "B X" => 1, "B Y" => 5, "B Z" => 9, "C X" => 7, "C Y" => 2, "C Z" => 6))
 day02(ARGS[1], Dict("A X" => 3, "A Y" => 4, "A Z" => 8, "B X" => 1, "B Y" => 5, "B Z" => 9, "C X" => 2, "C Y" => 6, "C Z" => 7))
