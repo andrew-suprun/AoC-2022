9593d819ff0cf091d556078983a86960121b3ba3
Andrew Suprun <andrew.suprun@gmail.com>
2022-12-03 01:00:11 -0500

    using Iterators.pertition

--- day03.jl
+++ day03.jl
@@ -1,3 +1,5 @@
+using .Iterators
+
 read_lines() = [line for line in readlines("day03.txt")]
 
 function split_in_half(line)
@@ -13,19 +15,6 @@ function priority(letter)
     end
 end
 
-function split_groups(lines)
-    i = 0
-    groups = Vector{Vector{String}}()
-    for line in lines
-        if mod(i, 3) == 0
-            push!(groups, [])
-        end
-        push!(groups[end], line)
-        i += 1
-    end
-    return groups
-end
-
 function day03a()
     lines = read_lines()
     pairs = [split_in_half(line) for line in lines]
@@ -36,7 +25,7 @@ end
 
 function day03b()
     lines = read_lines()
-    groups = split_groups(lines)
+    groups = partition(lines, 3)
     common_letters = [pop!(intersect(Set(group[1]), Set(group[2]), Set(group[3]))) for group in groups]
     priorities = [priority(letter) for letter in common_letters]
     println(sum(priorities))
