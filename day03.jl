885ed1571a7beb6a3d95f95d46b4c3cabbdf0609
Andrew Suprun <andrew.suprun@gmail.com>
2022-12-03 01:22:55 -0500

    use generators

--- day03.jl
+++ day03.jl
@@ -5,7 +5,7 @@ end
 
 priority(letter) = letter >= 'a' && letter <= 'z' ? letter - 'a' + 1 : letter - 'A' + 27
 
-print_priorities(letters) = println(sum([priority(letter) for letter in letters]))
+print_priorities(letters) = println(sum(priority(letter) for letter in letters))
 
 function day03a()
     lines = [line for line in readlines("day03.txt")]
@@ -14,7 +14,7 @@ function day03a()
 end
 
 function day03b()
-    groups = Base.Iterators.partition([line for line in readlines("day03.txt")], 3)
+    groups = Base.Iterators.partition((line for line in readlines("day03.txt")), 3)
     print_priorities([pop!(intersect(Set(group[1]), Set(group[2]), Set(group[3]))) for group in groups])
 end
 
