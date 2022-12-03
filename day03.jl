8947e31ebb36f32e141d98c9e89bc39f91de35eb
Andrew Suprun <andrew.suprun@gmail.com>
2022-12-03 01:13:16 -0500

    extract print_priorities

--- day03.jl
+++ day03.jl
@@ -2,24 +2,22 @@ using .Iterators
 
 function split_in_half(line)
     l = div(length(line), 2)
-    return SubString(line, 1, l), SubString(line, l + 1)
+    return line[1:l], line[l+1:end]
 end
 
 priority(letter) = letter >= 'a' && letter <= 'z' ? letter - 'a' + 1 : letter - 'A' + 27
 
+print_priorities(letters) = println(sum([priority(letter) for letter in letters]))
+
 function day03a()
     lines = [line for line in readlines("day03.txt")]
     pairs = [split_in_half(line) for line in lines]
-    common_letters = [pop!(intersect(Set(pair[1]), Set(pair[2]))) for pair in pairs]
-    priorities = [priority(letter) for letter in common_letters]
-    println(sum(priorities))
+    print_priorities([pop!(intersect(Set(pair[1]), Set(pair[2]))) for pair in pairs])
 end
 
 function day03b()
     groups = partition([line for line in readlines("day03.txt")], 3)
-    common_letters = [pop!(intersect(Set(group[1]), Set(group[2]), Set(group[3]))) for group in groups]
-    priorities = [priority(letter) for letter in common_letters]
-    println(sum(priorities))
+    print_priorities([pop!(intersect(Set(group[1]), Set(group[2]), Set(group[3]))) for group in groups])
 end
 
 day03a()
