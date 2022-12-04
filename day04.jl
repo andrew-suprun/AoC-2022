4d0202e400344222915b081a7d9588fae754b9c2
Andrew Suprun <andrew.suprun@gmail.com>
2022-12-04 01:53:29 -0500

    simplify day04 part 2

--- day04.jl
+++ day04.jl
@@ -4,4 +4,4 @@ day04(cond) = count(cond, readlines("day04.txt") .|> x -> split(x, ['-', ',']) .
 day04(((a1, a2, b1, b2),) -> a1 <= b1 <= b2 <= a2 || b1 <= a1 <= a2 <= b2)
 
 # part 2
-day04(((a1, a2, b1, b2),) -> a1 <= b1 <= a2 || a1 <= b2 <= a2 || b1 <= a1 <= b2 || b1 <= a2 <= b2)
+day04(((a1, a2, b1, b2),) -> a1 <= b1 <= a2 || a1 <= b2 <= a2 || b1 <= a1 <= b2)
