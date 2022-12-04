cd3ecc4025e6df68c226799e204bd91b6ca95a35
Andrew Suprun <andrew.suprun@gmail.com>
2022-12-04 13:49:45 -0500

    simplify day04

--- day04.jl
+++ day04.jl
@@ -1,7 +1,7 @@
 day04(cond) = count(cond, readlines("day04.txt") .|> x -> split(x, ['-', ',']) .|> x -> parse(Int, x)) |> println
 
 # part 1
-day04(((a1, a2, b1, b2),) -> a1 <= b1 <= b2 <= a2 || b1 <= a1 <= a2 <= b2)
+day04(((a1, a2, b1, b2),) -> issubset(a1:a2, b1:b2) || issubset(b1:b2, a1:a2))
 
 # part 2
-day04(((a1, a2, b1, b2),) -> a1 <= b1 <= a2 || a1 <= b2 <= a2 || b1 <= a1 <= b2)
+day04(((a1, a2, b1, b2),) -> length(intersect(a1:a2, b1:b2)) > 0)
