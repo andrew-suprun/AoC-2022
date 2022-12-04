d26577134572ebca895cac8b14e0b413a9c1ae6d
Andrew Suprun <andrew.suprun@gmail.com>
2022-12-04 01:50:33 -0500

    refactor day04

--- day04.jl
+++ day04.jl
@@ -1,24 +1,7 @@
-function day04a()
-    count = 0
-    for line in readlines("day04.txt")
-        (a1, a2, b1, b2) = parse.(Int, split(line, ['-', ',']))
-        if a1 <= b1 <= b2 <= a2 || b1 <= a1 <= a2 <= b2
-            count += 1
-        end
-    end
-    println(count)
-end
+day04(cond) = count(cond, readlines("day04.txt") .|> x -> split(x, ['-', ',']) .|> x -> parse(Int, x)) |> println
 
-function day04b()
-    count = 0
-    for line in readlines("day04.txt")
-        (a1, a2, b1, b2) = parse.(Int, split(line, ['-', ',']))
-        if a1 <= b1 <= a2 || a1 <= b2 <= a2 || b1 <= a1 <= b2 || b1 <= a2 <= b2
-            count += 1
-        end
-    end
-    println(count)
-end
+# part 1
+day04(((a1, a2, b1, b2),) -> a1 <= b1 <= b2 <= a2 || b1 <= a1 <= a2 <= b2)
 
-day04a()
-day04b()
+# part 2
+day04(((a1, a2, b1, b2),) -> a1 <= b1 <= a2 || a1 <= b2 <= a2 || b1 <= a1 <= b2 || b1 <= a2 <= b2)
