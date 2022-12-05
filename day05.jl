92a9b42568ba5dd9ce3f2f42cb95de39780f805f
Andrew Suprun <andrew.suprun@gmail.com>
2022-12-05 01:08:03 -0500

    refactor day05

--- day05.jl
+++ day05.jl
@@ -1,4 +1,4 @@
-function day05a()
+function day05(cranemover)
     lines = readlines("day05.txt")
     nline = 1
     while true
@@ -7,6 +7,7 @@ function day05a()
         end
         nline += 1
     end
+
     stacks = Vector{Vector{Char}}()
 
     for j in 2:4:length(lines[nline])
@@ -21,58 +22,32 @@ function day05a()
 
     for line in lines[nline+2:end]
         parts = split(line, " ")
-        nboxes = parse(Int, parts[2])
+        ncrates = parse(Int, parts[2])
         source = parse(Int, parts[4])
         target = parse(Int, parts[6])
-        for _ in 1:nboxes
-            box = pop!(stacks[source])
-            push!(stacks[target], box)
-        end
+        cranemover(stacks, ncrates, source, target)
     end
-    top_boxes = [last(stack) for stack in stacks]
-    @show join(top_boxes)
+    println(join(last(stack) for stack in stacks))
 end
 
-function day05b()
-    lines = readlines("day05.txt")
-    nline = 1
-    while true
-        if startswith(lines[nline], " 1 ")
-            break
-        end
-        nline += 1
+function cratemover_9000(stacks, ncrates, source, target)
+    for _ in 1:ncrates
+        crate = pop!(stacks[source])
+        push!(stacks[target], crate)
     end
-    stacks = Vector{Vector{Char}}()
-
-    for j in 2:4:length(lines[nline])
-        stack = Vector{Char}()
-        for k in nline-1:-1:1
-            if lines[k][j] != ' '
-                push!(stack, lines[k][j])
-            end
-        end
-        push!(stacks, stack)
-    end
-
-    for line in lines[nline+2:end]
-        parts = split(line, " ")
-        nboxes = parse(Int, parts[2])
-        source = parse(Int, parts[4])
-        target = parse(Int, parts[6])
-        tmp = Vector{Char}()
-        for _ in 1:nboxes
-            box = pop!(stacks[source])
-            push!(tmp, box)
-        end
-        for _ in 1:nboxes
-            box = pop!(tmp)
-            push!(stacks[target], box)
-        end
-    end
-    top_boxes = [last(stack) for stack in stacks]
-    @show join(top_boxes)
 end
 
-day05a()
-day05b()
+function cratemover_9001(stacks, ncrates, source, target)
+    tmp = Vector{Char}()
+    for _ in 1:ncrates
+        crate = pop!(stacks[source])
+        push!(tmp, crate)
+    end
+    for _ in 1:ncrates
+        crate = pop!(tmp)
+        push!(stacks[target], crate)
+    end
+end
 
+day05(cratemover_9000)
+day05(cratemover_9001)
