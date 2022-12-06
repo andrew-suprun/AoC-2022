53fdd613893a066eda0261f8d8c5de23419f4e9c
Andrew Suprun <andrew.suprun@gmail.com>
2022-12-06 00:25:15 -0500

    minor fix

--- day06.jl
+++ day06.jl
@@ -1,6 +1,6 @@
 function day06(marker_length)
     line = readline("day06.txt")
-    for i in 1:length(line)+marker_length-1
+    for i in 1:length(line)-marker_length+1
         if length(unique(line[i:i+marker_length-1])) == marker_length
             println(i + marker_length - 1)
             break
