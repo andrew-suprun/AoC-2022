77e43e072798da8c10e59f3f48509fba137f4e4f
Andrew Suprun <andrew.suprun@gmail.com>
2022-12-07 15:11:35 -0500

    refactor day07

--- day07.jl
+++ day07.jl
@@ -1,91 +1,96 @@
 mutable struct Dir
-    dirs::Vector{Dir}
-    files::Vector{Int}
+    name::String
+    subdirs::Vector{Dir}
+    size::Int
     parent::Union{Nothing,Dir}
-    Dir(; parent=nothing) = new(Vector{Dir}(), Vector{Tuple{String,Int}}(), parent)
+
+    Dir(name; parent=nothing) = new(name, Vector{Dir}(), 0, parent)
 end
 
-function command(line, pwd)
+function command(line, wd)
     cmd_line = split(line, ' ')
     if cmd_line[2] == "cd"
         name = cmd_line[3]
-        if name == "/"
-            while pwd.parent !== nothing
-                pwd = pwd.parent
-            end
-        elseif name == ".."
-            pwd = pwd.parent
-        else
-            subdir = Dir(parent=pwd)
-            push!(pwd.dirs, subdir)
-            pwd = subdir
+        if name == ".."
+            wd = wd.parent
+        elseif name != "/"
+            subdir = Dir(name, parent=wd)
+            push!(wd.subdirs, subdir)
+            wd = subdir
         end
     end
-    return pwd
+    return wd
 end
 
-function file(line, pwd)
+function file(line, wd)
     (size, _) = split(line, " ")
-    push!(pwd.files, parse(Int, size))
+    wd.size += parse(Int, size)
 end
 
-function scan(dir)
-    total = 0
-    for file in dir.files
-        total += file
-    end
-    for subdir in dir.dirs
-        size = scan(subdir)
-        total += size
-    end
-    return total
-end
-
-function walk_dirs(dir)
-    total = 0
-    for subdir in dir.dirs
-        size = scan(subdir)
-        if size <= 100000
-            total += size
-        end
-        total += walk_dirs(subdir)
-    end
-    return total
-end
-
-function find_suitable(dir, min_size)
-    total = 70_000_000
-    for subdir in dir.dirs
-        size = scan(subdir)
-        if size >= min_size && size < total
-            total = size
-        end
-    end
-    for subdir in dir.dirs
-        size = find_suitable(subdir, min_size)
-        if size >= min_size && size < total
-            total = size
-        end
-    end
-    return total
-end
-
-function day07()
-    root = Dir()
-    pwd = root
-    for line in readlines("day07.txt")
+function parse_input(path)
+    root = Dir("/")
+    wd = root
+    for line in readlines(path)
         if startswith(line, "\$ ")
-            pwd = command(line, pwd)
+            wd = command(line, wd)
         elseif !startswith(line, "dir")
-            file(line, pwd)
+            file(line, wd)
         end
     end
+    return root
+end
+
+function total_size(dir)
+    for subdir in dir.subdirs
+        dir.size += total_size(subdir)
+    end
+    return dir.size
+end
+
+function print_tree(dir; level=0)
+    println("$("    "^level)$(dir.name): $(dir.size)")
+    for subdir in dir.subdirs
+        print_tree(subdir, level=level + 1)
+    end
+end
+
+function sum_small_dirs(dir)
+    total = 0
+    for subdir in dir.subdirs
+        if subdir.size <= 100000
+            total += subdir.size
+        end
+        total += sum_small_dirs(subdir)
+    end
+    return total
+
+end
+
+function find_suitable_dir(dir, min_size, max_size)
+    for subdir in dir.subdirs
+        if subdir.size >= min_size && subdir.size < max_size
+            max_size = subdir.size
+        end
+        max_size = find_suitable_dir(subdir, min_size, max_size)
+    end
+    return max_size
+end
+
+function day07(path)
+    root = parse_input(path)
+    total_size(root)
+    print_tree(root)
 
     # part 1
-    println(walk_dirs(root))
+    println(sum_small_dirs(root))
 
     # part 2
-    println(find_suitable(root, scan(root) - 40_000_000))
+    println(find_suitable_dir(root, root.size - 40_000_000, 70_000_000))
 end
 
-day07()
+day07("day07.txt")
+
+# ----
+# 1325919
+# 2050735
+# ----
