2166d4b8acd3c98e8454ee917eef39a62627b447
Andrew Suprun <andrew.suprun@gmail.com>
2022-12-10 17:27:10 -0500

    day10

---
+++ day10.jl
@@ -0,0 +1,48 @@
+function day10(lines)
+    trace = [1]
+
+    noop = () -> push!(trace, trace[end])
+
+    function addx(v)
+        push!(trace, trace[end])
+        push!(trace, trace[end] + v)
+    end
+
+    ISA = Dict(
+        "noop" => noop,
+        "addx" => addx,
+    )
+
+    for line in lines
+        (code, params...) = split(line)
+        if code == "noop"
+            noop()
+        elseif code == "addx"
+            addx(parse(Int, params[1]))
+        end
+    end
+
+    # part 1: 
+    part1 = 0
+    for i in 20:40:length(trace)
+        part1 += i * trace[i]
+    end
+    println("Part 1: $part1") # 14620
+
+    # part 2:
+    print("Part 2:") # BJFRHRFU
+    for (cycle, x) in enumerate(trace)
+        pos = rem(cycle - 1, 40)
+        if pos == 0
+            println()
+        end
+        if abs(pos - x) <= 1
+            print("#")
+        else
+            print(".")
+        end
+    end
+    println()
+end
+
+day10(readlines("day10.txt"))
