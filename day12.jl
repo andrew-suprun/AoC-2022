fd3df3849fb0ac05ff56fd35fcfeea8e255c28b9
Andrew Suprun <andrew.suprun@gmail.com>
2022-12-12 14:48:43 -0500

    day12: simplification

--- day12.jl
+++ day12.jl
@@ -9,21 +9,21 @@ function day12(lines, part)
                 heightmap[l, r] = 0
                 start = (l, r)
             elseif lines[l][r] == 'E'
-                heightmap[l, r] = 25
+                heightmap[l, r] = 'z' - 'a'
                 target = (l, r)
             else
                 heightmap[l, r] = lines[l][r] - 'a'
             end
         end
     end
-    visited = Dict([start => start])
+    visited = Set([start])
     current_level::Vector{Tuple{Int,Int}} = [start]
 
     if part == :part2
         for l in eachindex(lines)
             for r in eachindex(lines[l])
                 if lines[l][r] == 'a'
-                    visited[(l, r)] = (l, r)
+                    push!(visited, (l, r))
                     push!(current_level, (l, r))
                 end
             end
@@ -36,27 +36,27 @@ function day12(lines, part)
         for (l, r) in current_level
             current_height = heightmap[l, r]
 
-            if r < nrows && heightmap[l, r+1] - 1 <= current_height && !(haskey(visited, (l, r + 1)))
-                visited[(l, r + 1)] = (l, r)
+            if r < nrows && heightmap[l, r+1] - 1 <= current_height && !((l, r + 1) in visited)
+                push!(visited, (l, r + 1))
                 push!(next_level, (l, r + 1))
             end
-            if r > 1 && heightmap[l, r-1] - 1 <= current_height && !(haskey(visited, (l, r - 1)))
-                visited[(l, r - 1)] = (l, r)
+            if r > 1 && heightmap[l, r-1] - 1 <= current_height && !((l, r - 1) in visited)
+                push!(visited, (l, r - 1))
                 push!(next_level, (l, r - 1))
             end
-            if l < nlines && heightmap[l+1, r] - 1 <= current_height && !(haskey(visited, (l + 1, r)))
-                visited[(l + 1, r)] = (l, r)
+            if l < nlines && heightmap[l+1, r] - 1 <= current_height && !((l + 1, r) in visited)
+                push!(visited, (l + 1, r))
                 push!(next_level, (l + 1, r))
             end
-            if l > 1 && heightmap[l-1, r] - 1 <= current_height && !(haskey(visited, (l - 1, r)))
-                visited[(l - 1, r)] = (l, r)
+            if l > 1 && heightmap[l-1, r] - 1 <= current_height && !((l - 1, r) in visited)
+                push!(visited, (l - 1, r))
                 push!(next_level, (l - 1, r))
             end
         end
 
         current_level = next_level
         steps += 1
-        if haskey(visited, target)
+        if target in visited
             break
         end
     end
@@ -67,7 +67,7 @@ using BenchmarkTools
 println(@btime day12(readlines("day12.txt"), :part1))
 println(@btime day12(readlines("day12.txt"), :part2))
 
-#   446.459 μs (1281 allocations: 1020.11 KiB)
+#   438.500 μs (1279 allocations: 678.64 KiB)
 # 472
-#   464.167 μs (1237 allocations: 1.06 MiB)
+#   454.500 μs (1235 allocations: 739.33 KiB)
 # 465
