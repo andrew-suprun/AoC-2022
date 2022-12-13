e2b35f869582ca6e913a0a85179533342bcef588
Andrew Suprun <andrew.suprun@gmail.com>
2022-12-13 16:31:46 -0500

    day13: refactoring

--- day13.jl
+++ day13.jl
@@ -1,92 +1,61 @@
-function tokenize(line)
-    tokens = Vector{Union{Int,Symbol}}()
+mutable struct List
+    items::Vector{Union{List,Int}}
+
+    List() = new(Union{List,Int}[])
+    List(i::Int) = new(Union{List,Int}[i])
+end
+
+function List(line::String)
+    stack = List[]
     start_number = 0
     for (i, ch) in enumerate(line)
         if ch ≥ '0' && ch ≤ '9' && start_number == 0
             start_number = i
         elseif (ch < '0' || ch > '9') && start_number > 0
-            push!(tokens, parse(Int, line[start_number:i-1]))
+            push!(stack[end].items, parse(Int, line[start_number:i-1]))
             start_number = 0
         end
         if ch == '['
-            push!(tokens, :open)
+            push!(stack, List())
         elseif ch == ']'
-            push!(tokens, :close)
+            top = pop!(stack)
+            isempty(stack) && return top
+            push!(stack[end].items, top)
         end
     end
-    return tokens
+    return stack[1]
 end
 
-function isless(left_source::Vector{Union{Int,Symbol}}, right_source::Vector{Union{Int,Symbol}})
-    left = copy(left_source)
-    right = copy(right_source)
-    left_index = right_index = 1
-    while true
-        left_token = left[left_index]
-        right_token = right[right_index]
-        if left_token == :open
-            if right_token == :open
-                left_index += 1
-                right_index += 1
-                continue
-            elseif right_token == :close
-                return false
-            else
-                insert!(right, right_index, :open)
-                insert!(right, right_index + 2, :close)
-                continue
-            end
-        elseif left_token == :close
-            if right_token == :open
-                return true
-            elseif right_token == :close
-                left_index += 1
-                right_index += 1
-                continue
-            else
-                return true
-            end
-        else
-            if right_token == :open
-                insert!(left, left_index, :open)
-                insert!(left, left_index + 2, :close)
-                continue
-            elseif right_token == :close
-                return false
-            else
-                if left_token < right_token
-                    return true
-                elseif left_token > right_token
-                    return false
-                else
-                    left_index += 1
-                    right_index += 1
-                    continue
-                end
-            end
-        end
+Base.show(io::IO, list::List) = (print(io, "["); join(io, list.items, ","); print(io, "]"))
+
+function Base.:(==)(left::List, right::List)
+    length(left.items) != length(right.items) && return false
+    for (l, r) in zip(left.items, right.items)
+        l != r && return false
     end
     return true
 end
+Base.:(==)(l::List, r::Int) = l == List(r)
+Base.:(==)(l::Int, r::List) = List(l) == r
 
-function day13a(lines) # TODO refactor
-    sum = 0
-    j = 0
-    for i in 1:3:length(lines)
-        inorder = isless(tokenize(lines[i]), tokenize(lines[i+1]))
-        j += 1
-        sum += j * inorder
+function Base.:(<)(left::List, right::List)
+    for (l, r) in zip(left.items, right.items)
+        l < r && return true
+        l > r && return false
     end
-    return sum
+    return length(left.items) < length(right.items)
 end
+Base.:(<)(l::List, r::Int) = l < List(r)
+Base.:(<)(l::Int, r::List) = List(l) < r
+
+day13a(lines) = sum(i for i in 1:(length(lines)+1)÷3 if List(lines[3i-2]) < List(lines[3i-1]))
 
 function day13b(lines)
-    tokens_two = tokenize("[[2]]")
-    tokens_six = tokenize("[[6]]")
-    token_lines = map(tokenize, filter(l -> l != "", lines))
-    push!(token_lines, tokens_two, tokens_six)
-    sort!(token_lines, lt=isless)
-    return prod(i for (i, tokens) in enumerate(token_lines) if tokens == tokens_two || tokens == tokens_six)
+    lists = map(List, filter(l -> l != "", lines))
+    list2, list6 = List("[[2]]"), List("[[6]]")
+    push!(lists, list2, list6)
+    sort!(lists, lt=<)
+    return prod(i for (i, list) in enumerate(lists) if list == list2 || list == list6)
 end
 
 using BenchmarkTools
@@ -94,7 +63,7 @@ lines = readlines("day13.txt")
 println(@btime day13a(lines))
 println(@btime day13b(lines))
 
-#   395.917 μs (7907 allocations: 754.98 KiB)
+#   473.625 μs (18530 allocations: 704.70 KiB)
 # 5013
-#   800.167 μs (14749 allocations: 5.05 MiB)
+#   718.125 μs (26097 allocations: 1014.45 KiB)
 # 25038
