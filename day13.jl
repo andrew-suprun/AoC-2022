53db22ea0c7d9305095615162d69c36202d4e81e
Andrew Suprun <andrew.suprun@gmail.com>
2022-12-13 18:07:28 -0500

    day13: refactoring

--- day13.jl
+++ day13.jl
@@ -28,25 +28,25 @@ end
 
 Base.show(io::IO, list::List) = (print(io, "["); join(io, list.items, ","); print(io, "]"))
 
-function Base.:(==)(left::List, right::List)
+function Base.isequal(left::List, right::List)
     length(left.items) != length(right.items) && return false
     for (l, r) in zip(left.items, right.items)
         l != r && return false
     end
     return true
 end
-Base.:(==)(l::List, r::Int) = l == List(r)
-Base.:(==)(l::Int, r::List) = List(l) == r
+Base.isequal(l::List, r::Int) = l == List(r)
+Base.isequal(l::Int, r::List) = List(l) == r
 
-function Base.:(<)(left::List, right::List)
+function Base.isless(left::List, right::List)
     for (l, r) in zip(left.items, right.items)
         l < r && return true
         l > r && return false
     end
     return length(left.items) < length(right.items)
 end
-Base.:(<)(l::List, r::Int) = l < List(r)
-Base.:(<)(l::Int, r::List) = List(l) < r
+Base.isless(l::List, r::Int) = l < List(r)
+Base.isless(l::Int, r::List) = List(l) < r
 
 day13a(lines) = sum(i for i in 1:(length(lines)+1)÷3 if List(lines[3i-2]) < List(lines[3i-1]))
 
@@ -54,7 +54,7 @@ function day13b(lines)
     lists = map(List, filter(l -> l != "", lines))
     list2, list6 = List("[[2]]"), List("[[6]]")
     push!(lists, list2, list6)
-    sort!(lists, lt=<)
+    sort!(lists)
     return prod(i for (i, list) in enumerate(lists) if list == list2 || list == list6)
 end
 
